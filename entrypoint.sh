d05d1663c3212f4e5cbeb3ceb7b41e77f8c007e7
Natan Camargos <natanscamargos@gmail.com>
2021-02-04 00:23:36 -0300

    Adding reindex param and removing logs

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-namecoind -regtest -datadir=.namecoin > /dev/null 2>&1
-sleep 5 
+namecoind -regtest -datadir=.namecoin -reindex -deprecatedrpc=accounts  > /dev/null 2>&1
+sleep 5
+namecoin-cli -rpcport=1112 -rpcuser=root -rpcpassword=root createwallet "" > /dev/null 2>&1 || true 
 namecoin-cli -rpcport=1112 -rpcuser=root -rpcpassword=root generatetoaddress 1 mgrGgK3gDCJp8osBUSPPC1ktRuKnxEBg93 > /dev/null 2>&1
 sleep 5
-namecoind -regtest -datadir=.namecoin-2 > /dev/null 2>&1
+namecoind -regtest -datadir=.namecoin-2 -reindex -deprecatedrpc=accounts  > /dev/null 2>&1
 echo "Namecoind started!"
 /bin/bash "$@"
